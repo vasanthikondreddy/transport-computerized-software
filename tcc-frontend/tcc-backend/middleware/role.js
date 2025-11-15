8d87d54531c82d86d692f7df5f9ff840de486927
vasanthikondreddy <k.vasanthi882005@gmail.com>
2025-11-15 16:05:51 +0530

    Delete tcc-frontend/tcc-backend directory

--- tcc-frontend/tcc-backend/middleware/role.js
+++
@@ -1,8 +0,0 @@
-module.exports = function requireRole(roles) {
-  return function (req, res, next) {
-    if (!req.user || !roles.includes(req.user.role)) {
-      return res.status(403).json({ message: 'Access denied: insufficient role' });
-    }
-    next();
-  };
-};
--- tcc-frontend/tcc-backend/models/Branch.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const branchSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: true },
-  manager: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
-module.exports = mongoose.model('Branch', branchSchema);
